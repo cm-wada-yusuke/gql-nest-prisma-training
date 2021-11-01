31184282a23169218a63d5815251f72f653db107
Yusuke Wada <wada.yusuke@classmethod.jp>
2021-11-02 06:56:25 +0900

    image deploy

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
-  await app.listen(3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
