b61369453d3609cbbe00b8afeb7ecf11b1311e2c
Yusuke Wada <wada.yusuke@classmethod.jp>
2021-11-04 15:52:59 +0900

    file-upload

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fmp = require('fastify-multipart');
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+  app.register(fmp);
+  app.enableCors();
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
 }
