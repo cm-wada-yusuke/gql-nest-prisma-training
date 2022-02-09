cd21c2b075e429fa7dfff583ea97fe882d4ec771
Yusuke Wada <wada.yusuke@classmethod.jp>
2022-02-10 00:03:48 +0900

    feat: deploy frontend

--- blog-deploy-cloud-run/infra/modules/cloud-build/cloud-build.tf
+++ blog-deploy-cloud-run/infra/modules/cloud-build/cloud-build.tf
@@ -26,3 +26,23 @@ resource "google_cloudbuild_trigger" "deploy-backend-training-app" {
     _ARTIFACT_REPOSITORY_IMAGE_NAME = "${var.region}-docker.pkg.dev/${var.gcp_project_id}/${var.backend_app_name}/blog-backend"
   }
 }
+
+
+resource "google_cloudbuild_trigger" "deploy-frontend-training-app" {
+  name        = "deploy-frontend-training-app"
+  description = "Next.jsアプリを Cloud Run へdeployする"
+  github {
+    owner = var.github_owner
+    name  = var.github_app_repo_name
+    push {
+      branch = "^main$"
+    }
+  }
+  included_files = ["blog-deploy-cloud-run/frontend/**"]
+  filename       = "blog-deploy-cloud-run/frontend/cloudbuild.yml"
+  substitutions = {
+    _REGION                         = var.region
+    _SERVICE_ACCOUNT                = var.cloud_run_service_account
+    _ARTIFACT_REPOSITORY_IMAGE_NAME = "${var.region}-docker.pkg.dev/${var.gcp_project_id}/${var.frontend_app_name}/blog-frontend"
+  }
+}
